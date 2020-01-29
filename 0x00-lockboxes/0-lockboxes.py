fe46464fb21832b6a3eb86a2556a5ea62ff6f093
jmtes <juliennetesoro@gmail.com>
2020-01-29 12:26:04 -0800

    Make pep8 compliant

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
-def canUnlockAll(boxes):
-  to_check = set([0])
-  checked = set()
 
-  while len(to_check):
-    current_box = to_check.pop()
-    for key in boxes[current_box]:
-      if key not in checked:
-        to_check.add(key)
-    checked.add(current_box)
-  
-  if len(checked) == len(boxes):
-    return True
-  return False
+def canUnlockAll(boxes):
+    to_check = set([0])
+    checked = set()
+
+    while len(to_check):
+        current_box = to_check.pop()
+        for key in boxes[current_box]:
+            if key not in checked:
+                to_check.add(key)
+        checked.add(current_box)
+
+    if len(checked) == len(boxes):
+        return True
+    return False
