f688dfce7858ad43c0b4b133e671e72cec6c1110
jmtes <juliennetesoro@gmail.com>
2020-01-29 12:23:40 -0800

    Remove dictionary

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-  unlocked = {
-    'to_check': set([0]),
-    'checked': set()
-  }
+  to_check = set([0])
+  checked = set()
 
-  while len(unlocked['to_check']):
-    current_box = unlocked['to_check'].pop()
+  while len(to_check):
+    current_box = to_check.pop()
     for key in boxes[current_box]:
-      if key not in unlocked['checked']:
-        unlocked['to_check'].add(key)
-    unlocked['checked'].add(current_box)
+      if key not in checked:
+        to_check.add(key)
+    checked.add(current_box)
   
-  if len(unlocked['checked']) == len(boxes):
+  if len(checked) == len(boxes):
     return True
   return False
