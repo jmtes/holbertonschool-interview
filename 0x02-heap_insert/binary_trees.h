522ea6cea9a5a4ef305d6993f70e13da15ee985c
jmtes <juliennetesoro@gmail.com>
2020-02-13 16:45:36 -0800

    Modify typedef

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,21 +1,8 @@
 #ifndef BINARY_TREES_H
 #define BINARY_TREES_H
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-typedef struct binary_tree_s
-{
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-} binary_tree_t;
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -25,17 +12,19 @@ typedef struct binary_tree_s
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-} heap_t;
+};
 
-void binary_tree_print(const binary_tree_t *);
-void heap_print(const heap_t *);
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+typedef struct binary_tree_s heap_t;
+typedef struct binary_tree_s binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *tree);
+heap_t *binary_tree_node(heap_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
 
 
