0c801d2a9833b246ae492be12039d15029097acb
jmtes <juliennetesoro@gmail.com>
2020-03-20 01:23:42 -0700

    Fix styling issues

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -3,28 +3,31 @@
 /**
  * check_cycle - Check if singly linked list has a cycle
  * @list: Pointer to linked list
- * 
+ *
  * Return: 1 if list has a cycle, 0 otherwise
  */
 int check_cycle(listint_t *list)
 {
-  listint_t *slow = NULL;
-  listint_t *fast = NULL;
+	listint_t *slow = NULL;
+	listint_t *fast = NULL;
 
-  if (!list) {
-    return (0);
-  }
+	if (!list)
+	{
+		return (0);
+	}
 
-  slow = list;
-  fast = list;
+	slow = list;
+	fast = list;
 
-  while (slow->next && fast->next->next) {
-    slow = slow->next;
-    fast = fast->next->next;
-    if (slow == fast) {
-      return (1);
-    }
-  }
+	while (slow->next && fast->next->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+		{
+			return (1);
+		}
+	}
 
-  return (0);
+	return (0);
 }
