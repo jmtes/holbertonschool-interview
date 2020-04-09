d8385e29cdb2802dd4177c1a248fac926fd233f2
jmtes <juliennetesoro@gmail.com>
2020-04-09 14:56:08 -0700

    Use bit manipulation

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -2,5 +2,29 @@
 
 
 def validUTF8(data):
-    invalid_chars = list(filter(lambda x: (x > 255), data))
-    return False if len(invalid_chars) else True
+    firstBit = 1 << 7
+    secondBit = 1 << 6
+
+    currentBytes = 0
+
+    for n in data:
+        if not currentBytes:
+            bitPointer = 1 << 7
+
+            while bitPointer & n:
+                currentBytes += 1
+                bitPointer = bitPointer >> 1
+
+            if not currentBytes:
+                continue
+
+            if not 1 < currentBytes <= 4:
+                return False
+
+        else:
+            if (not (n & firstBit)) or (n & secondBit):
+                return False
+
+        currentBytes -= 1
+
+    return currentBytes == 0
