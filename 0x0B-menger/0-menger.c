ce564fe9a233cc7e6d6989c090254c88591e5a9e
jmtes <juliennetesoro@gmail.com>
2020-04-23 00:49:08 -0700

    Add file

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,26 @@
+#include "menger.h"
+
+void menger(int level) {
+  int x, y, d;
+  int dim = 1;
+
+  if (level >= 0) {
+    for (x = 0; x < level; x++) {
+      dim *= 3;
+    }
+
+    for (x = 0; x < dim; x++) {
+      for (y = 0; y < dim; y++) {
+        for (d = dim / 3; d > 0; d /= 3) {
+          if ((x % (d * 3)) / d == 1 && (y % (d * 3)) / d == 1) break;
+        }
+        if (d) {
+          printf(" ");
+        } else {
+          printf("#");
+        }
+      }
+      printf("\n");
+    }
+  }
+}
