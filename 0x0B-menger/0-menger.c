a072d448b4f8c4a363fb10b81728695f54964eac
jmtes <juliennetesoro@gmail.com>
2020-04-23 01:03:13 -0700

    Fix style issues

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -1,26 +1,41 @@
 #include "menger.h"
 
-void menger(int level) {
-  int x, y, d;
-  int dim = 1;
+/**
+ * menger - Print 2D Menger Sponge
+ * @level: Depth of Menger Sponge to print
+ *
+ * Return: None
+ */
+void menger(int level)
+{
+	int x, y, d;
+	int dim = 1;
 
-  if (level >= 0) {
-    for (x = 0; x < level; x++) {
-      dim *= 3;
-    }
+	if (level >= 0)
+	{
+	for (x = 0; x < level; x++)
+	{
+		dim *= 3;
+	}
 
-    for (x = 0; x < dim; x++) {
-      for (y = 0; y < dim; y++) {
-        for (d = dim / 3; d > 0; d /= 3) {
-          if ((x % (d * 3)) / d == 1 && (y % (d * 3)) / d == 1) break;
-        }
-        if (d) {
-          printf(" ");
-        } else {
-          printf("#");
-        }
-      }
-      printf("\n");
-    }
-  }
+		for (x = 0; x < dim; x++)
+		{
+			for (y = 0; y < dim; y++)
+			{
+				for (d = dim / 3; d > 0; d /= 3)
+				{
+					if ((x % (d * 3)) / d == 1 && (y % (d * 3)) / d == 1)
+					break;
+				}
+				if (d)
+				{
+					printf(" ");
+				} else
+				{
+					printf("#");
+				}
+			}
+			printf("\n");
+		}
+	}
 }
