b4a3c834361dbf41c7fdacca4b7c33da3533e535
jmtes <juliennetesoro@gmail.com>
2020-05-21 15:11:37 -0700

    Add test functions

--- 0x0E-linear_skip/linear_skip.js
+++ 0x0E-linear_skip/linear_skip.js
@@ -3,6 +3,64 @@ const SkipListNode = {
   index: undefined, // index of node
   next: undefined, // pointer to next node
   express: undefined // pointer to next node in express lane
+};
+
+function linearSkip (list, val) {
+  return SkipListNode;
+}
+
+function initExpress (list, size) {
+  const step = parseInt(Math.sqrt(size));
+  let save = list;
+
+  for (let i = 0; i < size; ++i) {
+    console.log(i);
+    if (i % step === 0) {
+      save.express = list;
+      save = list;
+    }
+    if (list.next !== undefined) {
+      list = list.next;
+    } else {
+      break;
+    }
+  }
+}
+
+function createSkipList (array, size) {
+  let list;
+
+  let sizeIter = size - 1;
+  while (sizeIter) {
+    const node = Object.create(SkipListNode);
+    node.n = array[sizeIter];
+    node.index = sizeIter;
+    node.express = null;
+    node.next = list;
+    list = node;
+    sizeIter--;
+  }
+
+  initExpress(list, size);
+  return list;
+}
+
+function printSkipList (list) {
+  let node;
+
+  console.log('List:');
+  node = list;
+  while (node !== undefined) {
+    console.log(`Index[${node.index}] = ${node.n}`);
+    node = node.next;
+  }
+
+  console.log('Express Lane:');
+  node = list;
+  while (node !== null) {
+    console.log(`Index[${node.index}] = ${node.n}`);
+    node = node.express;
+  }
 }
 
 function main () {
@@ -15,7 +73,7 @@ function main () {
   let val = 53;
   let res = linearSkip(list, val);
   console.log(`Found ${val} at ${res.index}`);
-  
+
   val = 2;
   res = linearSkip(list, val);
   console.log(`Found ${val} at ${res.index}`);
@@ -24,3 +82,5 @@ function main () {
   res = linearSkip(list, val);
   console.log(`Found ${val} at ${res.index}`);
 }
+
+main();
