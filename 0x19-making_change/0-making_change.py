3246d7e73755dfb05d1776ce71bc3573d23d9abe
jmtes <juliennetesoro@gmail.com>
2020-08-20 13:01:18 -0700

    Add script

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' Define makeChange function. '''
+
+
+def makeChange(coins, total):
+    ''' Determine fewest number of coins needed to make amount `total`. 
+
+        Positional arguments:
+        coins -- list of coin denominations
+        total -- the total amount to make 
+
+        Return: Fewest amount of coins needed to make `total`, -1 if impossible
+        to do so '''
+
+    coins.sort(reverse=True)
+    num_coins = 0
+
+    for coin in coins:
+        if total % coin < total:
+            num_coins += total // coin
+            total = total % coin
+
+    return num_coins if total == 0 else -1
