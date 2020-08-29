503d8deaec981940d0ec0ca50779d284c1ea287b
jmtes <juliennetesoro@gmail.com>
2020-08-28 19:33:08 -0700

    Fix formatting errors

--- 0x1A-double_circular_linked_list/0-add_node.c
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -4,88 +4,88 @@
  * add_node_end - Add node to end of circular doubly linked list
  * @list: Pointer to address of list head
  * @str: String to put in new node
- * 
+ *
  * Return: Pointer to new node on success, NULL on failure
  */
 List *add_node_end(List **list, char *str)
 {
-  List *new_node = NULL;
-  char *new_str = NULL;
+	List *new_node = NULL;
+	char *new_str = NULL;
 
-  if (!list || !str)
-    return NULL;
+	if (!list || !str)
+		return (NULL);
 
-  new_node = malloc(sizeof(List));
-  if (!new_node)
-    return NULL;
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
 
-  new_str = strdup(str);
-  if (!new_str)
-  {
-    free(new_node);
-    return NULL;
-  }
+	new_str = strdup(str);
+	if (!new_str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-  new_node->str = new_str;
+	new_node->str = new_str;
 
-  if (!(*list))
-  {
-    new_node->next = new_node;
-    new_node->prev = new_node;
-    *list = new_node;
-  }
-  else
-  {
-    new_node->next = *list;
-    new_node->prev = (*list)->prev;
-    (*list)->prev = new_node;
-    new_node->prev->next = new_node;
-  }
+	if (!(*list))
+	{
+		new_node->next = new_node;
+		new_node->prev = new_node;
+		*list = new_node;
+	}
+	else
+	{
+		new_node->next = *list;
+		new_node->prev = (*list)->prev;
+		(*list)->prev = new_node;
+		new_node->prev->next = new_node;
+	}
 
-  return new_node;
+	return (new_node);
 }
 
 /**
  * add_node_begin - Add node to beginning of circular doubly linked list
  * @list: Pointer to address of list head
  * @str: String to put in new node
- * 
+ *
  * Return: Pointer to new node on success, NULL on failure
  */
 List *add_node_begin(List **list, char *str)
 {
-  List *new_node = NULL;
-  char *new_str = NULL;
+	List *new_node = NULL;
+	char *new_str = NULL;
 
-  if (!list || !str)
-    return NULL;
+	if (!list || !str)
+		return (NULL);
 
-  new_node = malloc(sizeof(List));
-  if (!new_node)
-    return NULL;
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
 
-  new_str = strdup(str);
-  if (!new_str)
-  {
-    free(new_node);
-    return NULL;
-  }
+	new_str = strdup(str);
+	if (!new_str)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-  new_node->str = new_str;
+	new_node->str = new_str;
 
-  if (!(*list))
-  {
-    new_node->next = new_node;
-    new_node->prev = new_node;
-  }
-  else
-  {
-    new_node->next = *list;
-    new_node->prev = (*list)->prev;
-    (*list)->prev = new_node;
-    new_node->prev->next = new_node;
-  }
+	if (!(*list))
+	{
+		new_node->next = new_node;
+		new_node->prev = new_node;
+	}
+	else
+	{
+		new_node->next = *list;
+		new_node->prev = (*list)->prev;
+		(*list)->prev = new_node;
+		new_node->prev->next = new_node;
+	}
 
-  *list = new_node;
-  return new_node;
+	*list = new_node;
+	return (new_node);
 }
