b8a795520ee5ac322c60714f1c9848ab7dd76e0b
jmtes <juliennetesoro@gmail.com>
2020-09-04 15:07:16 -0700

    Implement in C

---
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+
+int get_max(int *array, size_t size)
+{
+  int signed_size = (short)size;
+  int max = 0;
+  int i;
+
+  for (i = 0; i < signed_size; i++)
+    if (array[i] > max)
+      max = array[i];
+
+  return max;
+}
+
+void counting_sort(int *array, size_t size, int digit)
+{
+  int signed_size = (short)size;
+  int tmp[size];
+  int count[10] = {0};
+  int i;
+
+  for (i = 0; i < signed_size; i++)
+    count[(array[i] / digit) % 10] += 1;
+
+  for (i = 1; i < 10; i++)
+    count[i] += count[i - 1];
+
+  for (i = size - 1; i >= 0; i--)
+  {
+    tmp[count[(array[i] / digit) % 10] - 1] = array[i];
+    count[(array[i] / digit) % 10]--;
+  }
+
+  for (i = 0; i < signed_size; i++)
+    array[i] = tmp[i];
+}
+
+void radix_sort(int *array, size_t size)
+{
+  int max = get_max(array, size);
+  int digit;
+
+  for (digit = 1; max / digit > 0; digit *= 10)
+    counting_sort(array, size, digit);
+}
